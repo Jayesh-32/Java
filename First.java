5bcff2333dfdad42fc5a976f0320d43a833fdd10
Jayesh-32 <70850443+Jayesh-32@users.noreply.github.com>
2020-11-05 14:52:33 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,9 @@
+public class First {
+    public static void main(String args[]) {
+      int j=15;
+      int k=35;
+      int s=j+k;
+
+      System.out.println("Sum of j + k is = " + s);
+    }
+}
